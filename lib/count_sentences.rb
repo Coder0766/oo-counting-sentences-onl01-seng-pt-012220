53ddd5db5116452b5cd2c22b5d0984eef65ae257
Courtney DeRiggs <courtney.deriggs@gmail.com>
2020-02-13 15:24:56 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,28 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
+
 
   def question?
-
+    self.end_with?("?")
   end
+
 
   def exclamation?
-
+    self.end_with?("!")
   end
+
+
 
   def count_sentences
-
+    binding.pry
+    self.split do self.end_with?(".", "?", "!")
+      return 0 if self === "string"
+        else
+          return self.
+    end
   end
+
 end
